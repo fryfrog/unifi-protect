d26b5bfec225d57d416ad2d39f781d10a1f036d0
Donald Webster <fryfrog@gmail.com>
2019-04-30 19:59:14 -0700

    Add PUID and PGID.

--- run.sh
+++ run.sh
@@ -18,6 +18,33 @@ function graceful_shutdown {
 # Trap SIGTERM for graceful exit
 trap graceful_shutdown SIGTERM
 
+# Change user nobody's UID to custom or match unRAID.
+export PUID
+PUID=$(echo "${PUID}" | sed -e 's/^[ \t]*//')
+if [[ -n "${PUID}" ]]; then
+  echo "[info] PUID defined as '${PUID}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+  echo "[warn] PUID not defined (via -e PUID), defaulting to '99'" | ts '%Y-%m-%d %H:%M:%.S'
+  export PUID="999"
+fi
+
+# Set user unify-video to specified user id (non unique)
+usermod -o -u "${PUID}" unifi-protect &>/dev/null
+
+# Change group users to GID to custom or match unRAID.
+export PGID
+PGID=$(echo "${PGID}" | sed -e 's/^[ \t]*//')
+if [[ -n "${PGID}" ]]; then
+  echo "[info] PGID defined as '${PGID}'" | ts '%Y-%m-%d %H:%M:%.S'
+else
+  echo "[warn] PGID not defined (via -e PGID), defaulting to '100'" | ts '%Y-%m-%d %H:%M:%.S'
+  export PGID="999"
+fi
+
+# Set group users to specified group id (non unique)
+groupmod -o -g "${PGID}" unifi-protect &>/dev/null
+
+# Environment variables
 export $(grep -v '^#' /etc/default/unifi-protect | xargs)
 
 UFP_BACKUPS_DIR=/etc/unifi-protect/backups
